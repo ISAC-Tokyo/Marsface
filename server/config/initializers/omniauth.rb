9960b14acedce3d347ea75781132ebd6e9848eed
Hal Seki <hal@plants-web.jp>
2013-04-20 23:29:23 +0900

    added route

---
+++ server/config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook,"161768323988976","1c70c872560440dd3cb0598ecf27df0a"
+end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -55,6 +55,9 @@ Server::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => 'welcome#index'
 
+  #OmniAuth
+  match "/auth/:provider/callback" => "sessions#callback"
+  match "/logout" => "sessions#destroy", :as => :logout
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
