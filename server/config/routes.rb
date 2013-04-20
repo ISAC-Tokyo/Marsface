430c9ca147f7005610485623730db43d9e38a737
Hal Seki <hal@plants-web.jp>
2013-04-20 23:29:56 +0900

    added omniauth

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -60,6 +60,7 @@ Server::Application.routes.draw do
   match "/logout" => "sessions#destroy", :as => :logout
   # See how all your routes lay out with "rake routes"
 
+  get '/auth/:provider/callback', to: 'sessions#create'
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
