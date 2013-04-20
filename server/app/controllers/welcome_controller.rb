4a637d7fc128ccb6599573018b921cfb9958f29b
Hal Seki <hal@plants-web.jp>
2013-04-21 04:23:40 +0900

    implemented reload

--- server/app/controllers/welcome_controller.rb
+++ server/app/controllers/welcome_controller.rb
@@ -2,5 +2,17 @@ class WelcomeController < ApplicationController
   def index
     offset = rand(Image.count)
     @image = Image.first(:offset => offset)
+    @votes = @image.votes
+    @voted = false;
+    if (current_user)
+      myid = current_user.id
+      @votes.each do |v|
+       if (v.user_id == myid)
+         @voted = true
+         @myvote = v
+         break
+       end
+      end
+    end
   end
 end
