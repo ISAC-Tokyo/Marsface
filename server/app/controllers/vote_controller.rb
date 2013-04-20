e4fc97cfbb7b21aebc65a604ac4d78797f1ce370
Hal Seki <hal@plants-web.jp>
2013-04-21 04:10:55 +0900

    implemented voting

--- server/app/controllers/vote_controller.rb
+++ server/app/controllers/vote_controller.rb
@@ -2,8 +2,17 @@ class VoteController < ApplicationController
   def create
     uid = current_user.id
     unless (Vote.find(:first, :conditions => {:image_id => params[:image_id], :user_id => uid}))
-      Vote.create(:image_id => params[:image_id], :user_id => uid)
+      @vote = Vote.create(:image_id => params[:image_id], :user_id => uid)
     end
+    respond_to do |format|
+      format.html { render :layout => false }
+      format.js
+    end
+  end
+  def update
+    @vote = Vote.find(params[:id])
+    @vote.comment = params[:vote][:comment]
+    @vote.save
     respond_to do |format|
       format.html { render :layout => false }
       format.js
