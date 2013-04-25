730dacf23033248d513b92724a558c4cffccd08d
Hal Seki <hal@plants-web.jp>
2013-04-25 19:02:03 +0900

    refactering

--- server/app/models/image.rb
+++ server/app/models/image.rb
@@ -1,4 +1,10 @@
 class Image < ActiveRecord::Base
   has_many :votes
   attr_accessible :caption, :path
+  def voted?(id)
+    self.votes.any? {|v| v.user.id == id}
+  end
+  def votedby(id)
+    self.votes.each {|v| return v if v.user.id == id}
+  end
 end
--- server/app/models/vote.rb
+++ server/app/models/vote.rb
@@ -2,4 +2,7 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :image
   attr_accessible :comment, :image_id, :user_id, :num
+  def vto_s
+    (num == 1) ? "+" : "-"
+  end
 end
