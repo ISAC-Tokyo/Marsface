f4848f5fe9aacb79fcbf8678e977ac1b5fcd1abc
mu <mu@i-1615-29925-VM.cs64fcloud.internal>
2013-04-26 13:19:10 +0900

    bug fix

--- detector/ccv/bin/mktrainfilelist.py
+++ detector/ccv/bin/mktrainfilelist.py
@@ -5,5 +5,5 @@ import sys
 files = os.listdir(sys.argv[1])
 
 for file in files:
-    im = img.open(file)
+    im = img.open(sys.argv[1] + file)
     print os.path.abspath(file) + " " + str(im.size[0]) + " " + str(im.size[1])+ " " + str(im.size[0]) + " " + str(im.size[1])
