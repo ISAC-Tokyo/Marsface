3fab0c6edd4a31c014c571113f76bb75937cf43a
Hal Seki <hal@plants-web.jp>
2013-05-02 16:04:57 +0900

    added capfiles

--- server/config/production.rb
+++
@@ -1,5 +0,0 @@
-set :target_name, "production"
-role :app, "121.119.182.37"
-role :web, "121.119.182.37"
-role :db, "121.119.182.37", :primary => true
-set :branch, "release_phase1"
--- server/config/staging.rb
+++
@@ -1,5 +0,0 @@
-set :target_name, "staging"
-role :app, "dev2.hatsutabi.georepublic.net"
-role :web, "dev2.hatsutabi.georepublic.net"
-role :db, "dev2.hatsutabi.georepublic.net", :primary => true
-set :branch, :master
