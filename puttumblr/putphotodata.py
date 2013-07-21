37c7862b1790ae539d8fae2f8e4284c3f6ada91d
mitsuo yamamoto <mitsuo.yamamoto@gmail.com>
2013-07-21 19:11:51 +0900

    bug fix.

--- puttumblr/putphotodata.py
+++ puttumblr/putphotodata.py
@@ -2,40 +2,35 @@
 #coding: utf-8
 
 from tumblpy import Tumblpy
+import sys
+import csv
+import os.path
 
-CONSUMER_KEY = 'Musqb3f5NbD9TnXzoI3KkbIsOc6qbpAiW0d4HprSvZRxzLGUjj'
-CONSUMER_SECRET = 'Vp7pnxYxGwg3XfWhjSZFNCByQHtMUUrvGoJWTsMxmW7oxWM8bU'
-ACCESS_TOKEN = '91LC1vQuFzqM7B9kiU1oqLG1BItt43LmbFYWvCnLu2Ebg5ahKo'
-ACCESS_TOKEN_SECRET = '821sykPEGqxd2XiITiX5BasKAn8zJ91ITStyoj27YUPlOn2igw'
-BASE_HOSTNAME = 'marfaceproj.tumblr.com'
+#
 
 def main():
-    t = Tumblpy(app_key = CONSUMER_KEY,
-                app_secret = CONSUMER_SECRET,
-                oauth_token=ACCESS_TOKEN,
-                oauth_token_secret=ACCESS_TOKEN_SECRET)
-
-    blog_url = t.post('user/info')
-    blog_url = blog_url['user']['blogs'][0]['url']
-
-    files = open('/home/miyamamoto/work/Marsface/result/PIA08533_result.png', 'rb')
+    csvfile = sys.argv[1]
+    reader = csv.reader(file(csvfile, 'r'), delimiter   = ','  )
     
-    post = t.post('post', blog_url=blog_url, params={'type':'photo', 'caption': 'http://www.nikkei.co.jp', 'data':files})
+    for row in reader:
+        t = Tumblpy(app_key = CONSUMER_KEY,
+                    app_secret = CONSUMER_SECRET,
+                    oauth_token=ACCESS_TOKEN,
+                    oauth_token_secret=ACCESS_TOKEN_SECRET)
+        
+        blog_url = t.post('user/info')
+        blog_url = blog_url['user']['blogs'][0]['url']
+    
+        files = open('/home/miyamamoto/work/Marsface/result/PIA08533_result.png', 'rb')
+        url = os.path.splitext(row[1])[0].replace('tiff', 'catalog')
+        post = t.post('post', blog_url=blog_url, params={'type':'photo', 'caption': url, 'data':files})
 
-
-#    url = 'http://api.tumblr.com/v2/blog/%s/post' % BASE_HOSTNAME
-#    post_title = 'テ ス ト 投 稿 '
-#    post_body = 'こ れ は テ ス ト 投 稿 で す 。 '
-#    token = oauth2.Token(key=ACCESS_TOKEN, secret=ACCESS_TOKEN_SECRET)
-#    consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
-#    params = {
-#        'type': 'text',
-#        'state': 'draft',
-#        'title': post_title,
-#        'body': post_body,
-#    }
-#    client = oauth2.Client(consumer, token)
-#    resp, content = client.request(url, method='POST', body=urlencode(params))
+        url = " http://" +BASE_HOSTNAME + "/" +  str(post['id'])
+        print url
+        rows = [row[1],row[2],url]
+        writer = csv.writer(open("aaa.csv",'w'),lineterminator="¥t")
+        writer.writerows(rows)
+        
 
 if __name__ == '__main__':
     main()
