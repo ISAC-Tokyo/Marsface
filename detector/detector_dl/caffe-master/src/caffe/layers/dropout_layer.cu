03746581a81bcadcc60771b2773e6ccbdc9d0df0
Mitsuo YAMAMOTO <mitsuo.yamamoto@gmail.com>
2014-03-24 17:05:37 +0900

    add caffe

---
+++ detector/detector_dl/caffe-master/src/caffe/layers/dropout_layer.cu
@@ -0,0 +1,77 @@
+// Copyright 2013 Yangqing Jia
+
+#include <algorithm>
+#include <limits>
+#include <vector>
+
+#include "caffe/common.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/syncedmem.hpp"
+#include "caffe/vision_layers.hpp"
+
+using std::max;
+
+namespace caffe {
+
+
+template <typename Dtype>
+__global__ void DropoutForward(const int n, const Dtype* in,
+    const unsigned int* mask, const unsigned int threshold, const float scale,
+    Dtype* out) {
+  CUDA_KERNEL_LOOP(index, n) {
+    out[index] = in[index] * (mask[index] > threshold) * scale;
+  }
+}
+
+template <typename Dtype>
+void DropoutLayer<Dtype>::Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+    vector<Blob<Dtype>*>* top) {
+  const Dtype* bottom_data = bottom[0]->gpu_data();
+  Dtype* top_data = (*top)[0]->mutable_gpu_data();
+  const int count = bottom[0]->count();
+  if (Caffe::phase() == Caffe::TRAIN) {
+    CURAND_CHECK(curandGenerate(Caffe::curand_generator(),
+        (unsigned int*)(rand_vec_->mutable_gpu_data()), count));
+    // set thresholds
+    // NOLINT_NEXT_LINE(whitespace/operators)
+    DropoutForward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+        count, bottom_data, (unsigned int*)rand_vec_->gpu_data(), uint_thres_,
+        scale_, top_data);
+    CUDA_POST_KERNEL_CHECK;
+  } else {
+    CUDA_CHECK(cudaMemcpy(top_data, bottom_data,
+        count * sizeof(Dtype), cudaMemcpyDeviceToDevice));
+  }
+}
+
+template <typename Dtype>
+__global__ void DropoutBackward(const int n, const Dtype* in_diff,
+    const unsigned int* mask, const unsigned int threshold, const float scale,
+    Dtype* out_diff) {
+  CUDA_KERNEL_LOOP(index, n) {
+    out_diff[index] = in_diff[index] * scale * (mask[index] > threshold);
+  }
+}
+
+template <typename Dtype>
+Dtype DropoutLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
+    const bool propagate_down,
+    vector<Blob<Dtype>*>* bottom) {
+  CHECK(Caffe::phase() == Caffe::TRAIN);
+  if (propagate_down) {
+    const Dtype* top_diff = top[0]->gpu_diff();
+    Dtype* bottom_diff = (*bottom)[0]->mutable_gpu_diff();
+    const unsigned int* mask = (unsigned int*)rand_vec_->gpu_data();
+    const int count = (*bottom)[0]->count();
+    // NOLINT_NEXT_LINE(whitespace/operators)
+    DropoutBackward<Dtype><<<CAFFE_GET_BLOCKS(count), CAFFE_CUDA_NUM_THREADS>>>(
+        count, top_diff, mask, uint_thres_, scale_, bottom_diff);
+    CUDA_POST_KERNEL_CHECK;
+  }
+  return Dtype(0);
+}
+
+INSTANTIATE_CLASS(DropoutLayer);
+
+
+}  // namespace caffe
--- detector/makeenv/fabfile.py
+++ detector/makeenv/fabfile.py
@@ -21,6 +21,10 @@ tornado
 """
 def install_build():
     local("sudo apt-get install build-essential")
+    local("sudo apt-get install protobuf-compiler")
+    local("sudo apt-get install gdebi-core")
+    local("sudo apt-get install libdc1394-22-dev libdc1394-22 libdc1394-utils")
+    local("sudo apt-get -yV install libatlas-base-dev libatlas-dev libatlas-doc libatlas-test libatlas3gf-base")
 
 def install_bs():
     local("sudo pip install beautifulsoup")
@@ -37,9 +41,11 @@ def install_mathpkgs():
     local("sudo pip install ujson")
     local("sudo pip install boost_python")
 
+
 def install_matplotlib():
     local("sudo apt-get install python-matplotlib")
-
+    local("sudo pip install cython")
+    local("sudo apt-get install scikit-learn scikit-image")
 
 def install_ipython():
     local("sudo pip install ipython")
@@ -47,8 +53,17 @@ def install_ipython():
     local("sudo pip install jinja2")
     local("sudo pip install tornado --upgrade")
     
+def install_cuda():
+    local("wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/cuda-repo-ubuntu1204_5.5-0_amd64.deb")
+    local("sudo gdebi cuda-repo-ubuntu1204_5.5-0_amd64.deb")
+    local("sudo apt-get update")
+    local("sudo apt-get install cuda")
+
 def install_caffe():
     local("sudo sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev")
+
+
+
 
 def install_google_logging():
     local("wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz")
@@ -68,4 +83,5 @@ def demo1_deploy():
 #    install_matplotlib()
 #    install_ipython()
 #    install_caffe()
-    install_google_logging()
+#    install_google_logging()
+    install_cuda()
